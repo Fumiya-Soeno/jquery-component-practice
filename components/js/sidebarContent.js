1b16a483d8844f28e5c861605045469b45529300
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2021-05-15 23:25:15 +0900

    Update sidebarContent.js

--- components/js/sidebarContent.js
+++ components/js/sidebarContent.js
@@ -10,9 +10,9 @@ tagName = document.currentScript.src.split('/').pop(0).split('.')[0]
 $(tagName).parent().append(`<link rel="stylesheet" href="components/css/${tagName}.css">`)
 $(tagName).filter((i, dom) => {
     $(dom).html(`
-    <a href="" class="sidebar-content">
-        <i class="${$(tagName).attr('icon')}"></i>
-        <span>${$(tagName).attr('title')}</span>
+    <a href="${$(dom).attr('href')}" class="sidebar-content">
+        <i class="${$(dom).attr('icon')}"></i>
+        <span>${$(dom).attr('title')}</span>
     </a>
     `).children().unwrap()
 })
